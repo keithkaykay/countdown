751414aedb42fa24ebfd36ecd94de870bc5d1f12
Keith Kaykay <kaykaykeith@yahoo.com>
2021-08-15 23:43:31 +0300

    introduce cookies day one

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-function myFunction(){
-  var event = "doc";
-  document.getElementById("display").innerHTML = event;
+function getCookie(NameOfCookie)
+{ if (document.cookie.length > 0)
+{ begin = document.cookie.indexOf(NameOfCookie+"=");
+if (begin != -1)
+{ begin += NameOfCookie.length+1;
+end = document.cookie.indexOf(";", begin);
+if (end == -1) end = document.cookie.length;
+return unescape(document.cookie.substring(begin, end)); }
 }
+return null;
+}
+
+
+
+function setCookie(NameOfCookie, value, expiredays)
+{ var ExpireDate = new Date ();
+ExpireDate.setTime(ExpireDate.getTime() + (expiredays * 24 * 3600 * 1000));
+document.cookie = NameOfCookie + "=" + escape(value) +
+((expiredays == null) ? "" : "; expires=" + ExpireDate.toGMTString());
+}
+
+
+$(document).ready(function() {
+  $("button").click(function() {
+  $.get("jQuery.html", function(data,status) {
+  document.getElementById("jQuery").innerHTML = data;
+  });
+  });
+  });
